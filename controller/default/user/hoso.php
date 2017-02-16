6cd955b8a2cb866ecd2478533a010d1d9077c88f
tungtv <tungtv.soict@gmail.com>
2017-02-16 20:17:54 +0700

    up code

--- controller/default/user/hoso.php
+++ controller/default/user/hoso.php
@@ -36,7 +36,12 @@ if(isset($_POST['birthday'])&&isset($_POST['address_user'])&&isset($_POST['full_
     $address_user = _returnPostParamSecurity('address_user');
     $phong_ban = _returnPostParamSecurity('phong_ban');
     $chuc_vu = _returnPostParamSecurity('chuc_vu');
-    $guides = _returnPostParamSecurity('guides');
+     $guides = _returnPostParamSecurity('guides');
+    if ($guides === "on" || $guides === 1) {
+        $guides = 1;
+    } else {
+        $guides = 0;
+    }
     $guide_card_number = _returnPostParamSecurity('guide_card_number');
     $tax_code = _returnPostParamSecurity('tax_code');
     $user_phone = _returnPostParamSecurity('user_phone');
@@ -54,20 +59,72 @@ if(isset($_POST['birthday'])&&isset($_POST['address_user'])&&isset($_POST['full_
     $bank = _returnPostParamSecurity('bank');
     $open_bank = _returnPostParamSecurity('open_bank');
     $dan_toc = _returnPostParamSecurity('dan_toc');
-    $ton_giao = _returnPostParamSecurity('ton_giao');
+    $ton_giao = _returnPostParamSecurity('religion');
     $ho_khau_tt = _returnPostParamSecurity('ho_khau_tt');
     $language = _returnPostParamSecurity('language');
     $note = _returnPostParamSecurity('note');
-
     if ($full_name != '' && $birthday != '' && $address_user != '' && $user_phone != '' ) {
-
+        $array = (array)$data_user_update[0];
+        if($array['birthday']!='0000-00-00'){
+            $array['birthday']=date("Y-m-d", strtotime($array['birthday']));
+        }
+        if($array['ngay_lam_viec']!='0000-00-00'){
+            $array['ngay_lam_viec']=date("Y-m-d", strtotime($array['ngay_lam_viec']));
+        }
+        if($array['ngay_chinh_thuc']!='0000-00-00'){
+            $array['ngay_chinh_thuc']=date("Y-m-d", strtotime($array['ngay_chinh_thuc']));
+        }
+        $new_obj = new user($array);
+        $new_obj->mr=$mr;
+        $new_obj->name=$full_name;
+        if ($birthday != '') {
+            $new_obj->birthday=date("Y-m-d", strtotime($birthday));
+        }
+        $new_obj->gender=$gender;
+        $new_obj->address=$address_user;
+        $new_obj->phong_ban=$phong_ban;
+        $new_obj->chuc_vu=$chuc_vu;
+        $new_obj->guides=$guides;
+        $new_obj->guide_card_number=$guide_card_number;
+        $new_obj->tax_code=$tax_code;
+        $new_obj->phone=$user_phone;
+        $new_obj->mobi=$mobi;
+        $new_obj->skype=$skype;
+        $new_obj->facebook=$facebook;
+        $new_obj->cmnd=$cmnd;
+        if ($date_range_cmnd != '') {
+            $new_obj->date_range_cmnd=date("Y-m-d", strtotime($date_range_cmnd));
+        }
+        $new_obj->issued_by_cmnd=$issued_by_cmnd;
+        $new_obj->number_passport=$number_passport;
+        $new_obj->issued_by_passport=$issued_by_passport;
+        if ($date_range_passport != '') {
+            $new_obj->date_range_passport=date("Y-m-d", strtotime($date_range_passport));
+        }
+        if ($expiration_date_passport != '') {
+            $new_obj->expiration_date_passport=date("Y-m-d", strtotime($expiration_date_passport));
+        }
+        $new_obj->account_number_bank=$account_number_bank;
+        $new_obj->bank=$bank;
+        $new_obj->open_bank=$open_bank;
+        $new_obj->dan_toc=$dan_toc;
+        $new_obj->religion=$ton_giao;
+        $new_obj->ho_khau_tt=$ho_khau_tt;
+        $new_obj->language=$language;
+        $new_obj->note=$note;
+        $new_obj->updated = _returnGetDateTime();
+        $new_obj->id = $_SESSION['user_id'];
+        $folder = LocDau($data_user_update[0]->user_email);
+        $target_dir = _returnFolderRoot() . "/view/default/themes/uploads/" . $folder . '/';
+        $avatar = _returnUploadImg($target_dir, 'avatar', "/view/default/themes/uploads/" . $folder . '/');
+        if ($avatar != '') {
+            $new_obj->avatar=$avatar;
+        }
+        user_update($new_obj);
+        $data['data_user']=user_getById($_SESSION['user_id']);
     }else{
         echo '<script>alert("Bạn vui lòng điền đầy đủ thông tin bắt buộc")</script>';
     }
-
-
-}else{
-    echo '<script>alert("Bạn vui lòng điền đầy đủ thông tin bắt buộc")</script>';
 }
 show_header($data);
 show_left($data,'user','user_list');

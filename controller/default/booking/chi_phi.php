31a8d28cb8e8da412bcc8a88c0b9f26e26b78fda
tungtv <tungtv.soict@gmail.com>
2017-05-22 20:40:38 +0700

    up code

--- controller/default/booking/chi_phi.php
+++ controller/default/booking/chi_phi.php
@@ -27,13 +27,13 @@ $_SESSION['link_redict']='http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']
 $action_link=str_replace('?id='.$id,'',$action_link);
 $action_link=str_replace('chi-phi','',$action_link);
 $action_link=str_replace('/','',$action_link);
-//$data_dk_fill='booking_id = '.$id_pass;
+$data_dk_list='booking_id = '.$id_pass;
 $data_dk_fill='';
 $active_tab_left='booking_list';
 if($_SESSION['user_role']==0)
 {
-    $data_dk_fill.=' and user_id='.$_SESSION['user_id'];
-//    $data_dk_fill_list.=' and user_id='.$_SESSION['user_id'];
+    $data_dk_fill.=' and created_by='.$_SESSION['user_id'];
+    $data_dk_list.=' and user_id='.$_SESSION['user_id'];
 }
 
 switch($action_link){
@@ -90,6 +90,30 @@ switch($action_link){
         $active_tab_left='booking_list';
 
 }
+if(isset($_POST['name_gia'])&&isset($_POST['price_cost'])&&isset($_POST['created'])){
+    $name_gia= _returnPostParamSecurity('name_gia');
+    $price_cost= _returnPostParamSecurity('price_cost');
+    $created= _returnPostParamSecurity('created');
+    $description= _returnPostParamSecurity('description');
+    if ($name_gia != "" && $price_cost != '' && $created != '') {
+        if (isset($_POST['check_edit']) && isset($_POST['id_edit']) && $_POST['check_edit'] === "edit" && $_POST['id_edit'] != '') {
+
+        }else{
+            $obj = new booking_cost();
+            $obj->booking_id=$id_pass;
+            $obj->user_id=$_SESSION['user_id'];
+            $obj->name=$name_gia;
+            $obj->price=$price_cost;
+            $obj->description=$description;
+            $obj->created=$created;
+            $obj->created_by=$_SESSION['user_id'];
+            booking_cost_insert($obj);
+            redict(SITE_NAME . '/'.$action_link.'/chi-phi?id='.$id);
+        }
+    }else{
+        echo '<script>alert("Bạn vui lòng điền đầy đủ thông tin")</script>';
+    }
+}
 $data['dk_find'] =$data_dk_fill;
 $url_bread='<li class="active">Booking</li>';
 $data['breadcrumbs']=$url_bread;
@@ -97,7 +121,7 @@ $data['action_link']=$action_link;
 $count=11;
 
 
-$data['list']=booking_cost_getByTop('',$data_dk_fill,'updated desc');
+$data['list']=booking_cost_getByTop('',$data_dk_list,'updated desc');
 $data['module_valid'] = "booking";
 $data['title_print'] = $data['title'];
 show_header($data);

52c5208583c878486603d803eeeebfbc4f96dab3
tungtv <tungtv.soict@gmail.com>
2017-11-02 21:25:39 +0700

    up code

--- controller/default/booking/save_giao_dich.php
+++ controller/default/booking/save_giao_dich.php
@@ -7,6 +7,18 @@ if (isset($_POST['id']) && isset($_POST['code']) && isset($_POST['value'])) {
     $id = _return_mc_decrypt(_returnPostParamSecurity('id'), ENCRYPTION_KEY);
     $code = _returnPostParamSecurity('code');
     $value = _returnPostParamSecurity('value');
+    $created = _returnPostParamSecurity('created');
+    $time = _returnPostParamSecurity('time');
+    if($time==''){
+        $time=date("H:i:s", strtotime(_returnGetDateTime()));
+    }
+    if($created==''){
+        $created=_returnGetDateTime();
+    }else{
+        $created=date("Y-m-d", strtotime($created));
+    }
+    $created=$created.' '.$time;
+
      if($id && $code && $value){
 
         $data_check=booking_getById($id);
@@ -15,7 +27,7 @@ if (isset($_POST['id']) && isset($_POST['code']) && isset($_POST['value'])) {
              $obj->user_id=$_SESSION['user_id'];
              $obj->booking_id=$id;
              $obj->description=$value;
-             $obj->created = _returnGetDateTime();
+             $obj->created = $created;
              $obj->updated = _returnGetDateTime();
              booking_transactions_insert($obj);
              echo _returnListGiaodich($id);

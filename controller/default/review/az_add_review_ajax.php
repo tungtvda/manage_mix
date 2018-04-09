8fca06cf495dbd067a7ff0f366c68a7e488638e4
nuongphui <nuongphui.nhungchuyendi@gmail.com>
2018-04-09 21:33:47 +0700

    tungtv upcode

---
+++ controller/default/review/az_add_review_ajax.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tungtv
+ * Date: 11/10/14
+ * Time: 2:40 PM
+ */
+if (!defined('SITE_NAME')) {
+    require_once '../../../config.php';
+}
+require_once DIR . '/controller/default/public.php';
+
+$res = array(
+    'success' => 0,
+    'string_noti' => '',
+);
+
+print_r($_POST);
+exit;
